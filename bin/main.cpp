58993bfdbcea5c30c6b13731fc5985a5d8d6ddbf
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-05-29 12:57:22 +0200

    adding reading

--- bin/main.cpp
+++ bin/main.cpp
@@ -2,7 +2,95 @@
  #include <config.hpp>
 #endif
 
+#include <complex>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
+//coordinates in the lattice
+using coords_t=array<int,4>;
+
+//complex double
+using dcompl=complex<double>;
+
+//propagator (12X12)
+using prop_t=Matrix<dcompl,12,12>;
+
+//list of propagators
+using vprop_t=vector<prop_t>;
+
+//list of momenta
+vector<coords_t> mom_list;
+
+//read a file
+void read_mom_list(const string &path)
+{
+  //file
+  ifstream input(path);
+  if(!input.good())
+    {
+      cerr<<"Error opening "<<path<<endl;
+      exit(1);
+    }
+  
+  //loop until end of file
+  while(!input.eof())
+    {
+      coords_t c;
+      for(int mu=0;mu<4;mu++) input>>c[mu];
+      if(input.good()) mom_list.push_back(c);
+    }
+}
+
+//returns the linearized spin color index
+size_t isc(size_t is,size_t ic)
+{return ic+3*is;}
+
+//read a propagator file
+vprop_t read_prop(const string &path)
+{
+  vprop_t out(mom_list.size());
+  
+  ifstream input(path,ios::binary);
+  if(!input.good())
+    {
+      cerr<<"Unable to open file "<<path<<endl;
+      exit(1);
+    }
+  
+  for(int id_so=0;id_so<4;id_so++)
+    for(int ic_so=0;ic_so<3;ic_so++)
+      for(size_t imom=0;imom<mom_list.size();imom++)
+	for(int id_si=0;id_si<4;id_si++)
+	  for(int ic_si=0;ic_si<3;ic_si++)
+	    {
+	      double temp[2];
+	      input.read((char*)&temp,sizeof(double)*2);
+	      if(not input.good())
+		{
+		  cerr<<"Unable to read from "<<path<<" id_so: "<<id_so<<", ic_so: "<<ic_so<<", imom: "<<imom<<", id_si: "<<id_si<<", ic_si:"<<ic_si<<endl;
+		  exit(1);
+		}
+	      out[imom](isc(id_si,ic_si),isc(id_so,ic_so))=dcompl(temp[0],temp[1]); //store
+	    }
+  
+  return out;
+}
+
 int main(int narg,char **arg)
 {
+  read_mom_list("mom_list.txt");
+  
+  vprop_t S=read_prop("out/fft_SPECT0");
+  cout<<S[16]<<endl;
+  
+  S[0].inverse();
+  
+  S[0]*S[16];
+  
   return 0;
 }
