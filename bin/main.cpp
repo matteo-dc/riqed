c8f1da10cf2de9dfe9accb2e83077a5909dbee65
Matteo Di Carlo <matteo@MBP-di-Matteo-3.homenet.telecomitalia.it>
2017-07-16 17:22:54 +0200

    debug: uncommenting pragma omp

--- bin/main.cpp
+++ bin/main.cpp
@@ -256,13 +256,13 @@ jprop_t jackknife_prop(jprop_t &jS, int nconf, int clust_size, size_t nhits)
   valarray<prop_t> jSum(prop_t::Zero(),nmr);
 
   //sum of jS
-  //#pragma omp parallel for collapse(2)
+#pragma omp parallel for collapse(2)
   for(size_t j=0;j<jS.size();j++)
     for(int mr=0;mr<nmr;mr++)
       jSum[mr]+= jS[j][mr];
   
   //jackknife fluctuation
-  //#pragma omp parallel for collapse(2)
+#pragma omp parallel for collapse(2)
   for(size_t j=0;j<jS.size();j++)
     for(int mr=0;mr<nmr;mr++)
       jS[j][mr]=(jSum[mr]-jS[j][mr])/((nconf-clust_size)/nhits);
