a01892e0e2e6c2d8fccf27cb4c4ea443868032c4
Matteo Di Carlo <matteo@dot1x188.roma1.infn.it>
2017-07-18 12:30:25 +0200

    modified term gnuplot

--- bin/grapher.cpp
+++ bin/grapher.cpp
@@ -198,7 +198,7 @@ void plot_Zq(vector<jZ_t> jZq, vector<double> p2_vector, const string &name, con
   scriptfile<<"set autoscale xy"<<endl;
   scriptfile<<"set xlabel '$\\tilde{p}^2$'"<<endl;
   scriptfile<<"set ylabel 'Z_q'"<<endl;
-  scriptfile<<"set terminal png size 1200,800 enhanced font 'Helvetica,20'"<<endl;
+  scriptfile<<"set terminal latex"<<endl;
   if(strcmp(all_or_eq_moms.c_str(),"allmoms")==0) scriptfile<<"set output 'allmoms/"<<name<<".png'"<<endl;
   else if(strcmp(all_or_eq_moms.c_str(),"eqmoms")==0) scriptfile<<"set output 'eqmoms/"<<name<<".png'"<<endl;
   scriptfile<<"plot 'plot_"<<name<<"_"<<all_or_eq_moms<<"_data.txt' u 1:2:3 with errorbars pt 6 lc rgb 'blue'"<<endl;
