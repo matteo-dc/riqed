99e56d7c74a26d56fa6184d6fa98ce9210ac1f50
Matteo Di Carlo <matteo@MBP-di-Matteo-3.lan>
2017-09-18 00:30:42 +0200

    ranges

--- bin/grapher_with_fit.cpp
+++ bin/grapher_with_fit.cpp
@@ -398,7 +398,8 @@ void plot_Zq_sub(vector<jZ_t> &jZq, vector<jZ_t> &jZq_sub, vector<double> &p2_ve
     scriptfile<<"set xlabel '$a^2\\tilde{p}^2$'"<<endl;
     scriptfile<<"set ylabel '$Z_q$'"<<endl;
     scriptfile<<"set xrange [0:2.5]"<<endl;
-    // scriptfile<<"set yrange [*:*]"<<endl;
+    //  if(name=="Sigma1_em_correction")
+    //  scriptfile<<"set yrange [-0.17:-0.11]"<<endl;
     scriptfile<<"plot 'plot_data_and_script/plot_"<<name<<"_"<<all_or_eq_moms<<"_data.txt' u 1:2:3 with errorbars pt 6 lc rgb 'blue' title '$Z_q$'"<<endl;
     scriptfile<<"replot 'plot_data_and_script/plot_"<<name<<"_sub_"<<all_or_eq_moms<<"_data.txt' u 1:2:3 with errorbars pt 6 lt 1 lc rgb 'red' title '$Z_q^{\\mathrm{corr.}}$'"<<endl;
     scriptfile<<"set terminal epslatex color"<<endl;
@@ -604,8 +605,11 @@ void plot_Z_sub(vector<jZbil_t> &jZ, vector<jZbil_t> &jZ_sub, vector<double> &p2
 	scriptfile[i]<<"set xrange [0:2.5]"<<endl;
 	if(i==0 && name=="Z1")scriptfile[i]<<"set yrange [*:0.82]"<<endl;
 	if(i==2 && name=="Z1")scriptfile[i]<<"set yrange [*:0.55]"<<endl;
+	
 	if(i==0 && name=="Z1_em_correction")scriptfile[i]<<"set yrange [-0,28:-0.04]"<<endl;
-
+	if(i==1 && name=="Z1_em_correction")scriptfile[i]<<"set yrange [-0,12:-0.07]"<<endl;
+	if(i==3 && name=="Z1_em_correction")scriptfile[i]<<"set yrange [-0.17:-0.11]"<<endl;
+	
 	// scriptfile[i]<<"set yrange [0.7:0.9]"<<endl;
         scriptfile[i]<<"set ylabel '$Z_"<<bil[i]<<"$'"<<endl;
         scriptfile[i]<<"plot 'plot_data_and_script/plot_"<<name<<"_"<<bil[i]<<"_"<<all_or_eq_moms<<"_data.txt' u 1:2:3 with errorbars pt 6 lc rgb 'blue' title '$Z_"<<bil[i]<<"$'"<<endl;
