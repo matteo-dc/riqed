9bd4257c3663856502c9905e1abd559b028e0608
Matteo Di Carlo <matteo@MacBook-Pro-di-Matteo-3.local>
2017-11-29 19:08:17 +0100

    added ZP/ZS subtracted

--- bin/grapher_with_fit_swap.cpp
+++ bin/grapher_with_fit_swap.cpp
@@ -765,6 +765,50 @@ void plot_Z_sub(vector<vvd_t> &jZ, vector<vvd_t> &jZ_sub, vector<double> &p2_vec
         string command="gnuplot plot_data_and_script/plot_"+name+"_"+bil[i]+"_"+all_or_eq_moms+"_script.txt";
         
         system(command.c_str());
+        
+        
+        int moms=p2_vector.size();
+        int njacks=jZ.size();
+        vector<vd_t> jZP_over_S(moms,vd_t(0.0,njacks));
+        
+#pragma omp parallel for collapse(2)
+        for(int imom=0;imom<moms;imom++)
+            for(int ijack=0;ijack<njacks;ijack++)
+                jZP_over_S[imom][ijack]=jZ_sub[imom][ijack][2]/jZ_sub[imom][ijack][0];
+        
+        vvd_t ZP_over_S=average_Zq_chiral(jZP_over_S);
+        
+        ofstream datafile2;
+        
+        datafile2.open("plot_data_and_script/plot_"+name+"_P_over_S_"+all_or_eq_moms+"_data.txt");
+        
+        for(size_t imom=0;imom<p2_vector.size();imom++)
+            datafile2<<p2_vector[imom]<<"\t"<<ZP_over_S[0][imom]<<"\t"<<ZP_over_S[1][imom]<<endl;
+        
+        datafile2.close();
+        
+        
+        ofstream scriptfile2;
+        
+        scriptfile2.open("plot_data_and_script/plot_"+name+"_P_over_S_"+all_or_eq_moms+"_script.txt");
+        scriptfile2<<"set autoscale xy"<<endl;
+        scriptfile2<<"set xlabel '$a^2\\tilde{p}^2$'"<<endl;
+        // scriptfile2<<"set yrange [0.7:0.9]"<<endl;
+        scriptfile2<<"set ylabel '$Z_P/Z_S$'"<<endl;
+        scriptfile2<<"plot 'plot_data_and_script/plot_"<<name<<"_P_over_S_"<<all_or_eq_moms<<"_data.txt' u 1:2:3 with errorbars pt 6 lc rgb 'blue' title '$Z_P/Z_S$ chiral'"<<endl;
+        scriptfile2<<"set terminal epslatex color"<<endl;
+        if(strcmp(all_or_eq_moms.c_str(),"allmoms")==0) scriptfile2<<"set output 'allmoms/"<<name<<"_P_over_S.tex'"<<endl;
+        else if(strcmp(all_or_eq_moms.c_str(),"eqmoms")==0) scriptfile2<<"set output 'eqmoms/"<<name<<"_P_over_S.tex'"<<endl;
+        scriptfile2<<"replot"<<endl;
+        scriptfile2<<"set term unknown"<<endl;
+        
+        scriptfile2.close();
+        
+        string command2="gnuplot plot_data_and_script/plot_"+name+"_P_over_S_"+all_or_eq_moms+"_script.txt";
+        
+        system(command2.c_str());
+        
+        
     }
     
 }
@@ -1054,7 +1098,6 @@ void plot_Z_chiral(vector<vvd_t> &jZ_chiral, vector<double> &p2_vector, const st
     vvd_t ZP_over_S=average_Zq_chiral(jZP_over_S);
     
     ofstream datafile2;
-    
     
     datafile2.open("plot_data_and_script/plot_"+name+"_P_over_S_"+all_or_eq_moms+"_data.txt");
     
