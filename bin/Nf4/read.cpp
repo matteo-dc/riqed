66adaedae1029089789677543e7d4974dbc1b1e1
Matteo Di Carlo <matteo@dot1x151.roma1.infn.it>
2018-01-22 16:24:26 +0100

    fixed BC per<->antiper

--- bin/Nf4/read.cpp
+++ bin/Nf4/read.cpp
@@ -27,8 +27,8 @@ void read_mom_list(const string &path)
         double p_sqr=0.0, p_tilde_sqr=0.0;
         double p_4=0.0, p_tilde_4=0.0;
         p_t shift;
-        if(BC_str.compare("Periodic")==0) shift={0.5,0.0,0.0,0.0};
-        if(BC_str.compare("Antiperiodic")==0) shift={0.0,0.0,0.0,0.0};
+        if(BC_str.compare("Periodic")==0) shift={0.0,0.0,0.0,0.0};
+        if(BC_str.compare("Antiperiodic")==0) shift={0.5,0.0,0.0,0.0};
         
         for(int mu=0;mu<4;mu++)
         {
