4b649f9864392bb0ef13c7694fa4b7424517c76d
Matteo Di Carlo <matteo@MacBook-Pro-di-Matteo-3.local>
2018-02-01 16:02:48 +0100

    added comments

--- bin/main.cpp
+++ bin/main.cpp
@@ -55,24 +55,25 @@ int main()
     }
     
     char path[128]="input.txt";
-    read_input(path);
+    read_general_input(path);
     
     //read mom list
     read_mom_list(mom_path);
     moms=mom_list.size();
     cout<<"Read: "<<mom_list.size()<<" momenta from \""<<mom_path<<"\" (BC: "<<BC_str<<")."<<endl<<endl;
     
-    oper_t basic;
-    
     PRINT(p2_tilde);
+
+    /************************************************************************/
+    /**                at fixed beta, sea mass and theta                   **/
+    /************************************************************************/
+    
+    /*  basic  */
     
     deltam_cr=read_deltam_cr("deltam_cr_array");
     if(UseEffMass) eff_mass=read_eff_mass("eff_mass_array");
     
-//    basic.step="basic";
-    
-    /*  basic  */
-    
+    oper_t basic;
     basic.create_basic();
     basic.plot("");
     oper_t ave = basic.average_equiv_moms();
@@ -85,33 +86,48 @@ int main()
     oper_t rave_ave = rave.average_equiv_moms();  ///
     rave_ave.plot("rave_ave");
     
-    /*  chiral extr  */
+    /*  valence chiral extr  */
 
     oper_t rave_chir = rave.chiral_extr();
     rave_chir.plot("rave_chir");
     oper_t rave_chir_ave = rave_chir.average_equiv_moms();  ///
     rave_chir_ave.plot("rave_chir_ave");
     
-
     /*  O(a2g2) subtraction  */
-
+    
     oper_t rave_chir_sub = rave_chir.subtract();
     rave_chir_sub.plot("rave_chir_sub");
     oper_t rave_chir_sub_ave = rave_chir_sub.average_equiv_moms();  ///
     rave_chir_sub_ave.plot("rave_chir_sub_ave");
 
+    
+    /************************************************************************/
+    /**                     at fixed beta  and theta                       **/
+    /************************************************************************/
+
+    /*  sea chiral extr   */
+    
+    /************************************************************************/
+    /**                          at fixed beta                             **/
+    /************************************************************************/
+
+    /*  theta average   */
+    
+    
     /*  evolution to 1/a  */
     
     oper_t rave_chir_sub_evo = rave_chir_sub.evolve();
     rave_chir_sub_evo.plot("rave_chir_sub_evo");
     oper_t rave_chir_sub_evo_ave = rave_chir_sub_evo.average_equiv_moms();
     rave_chir_sub_evo_ave.plot("rave_chir_sub_evo_ave");
-
-    /*  a2p2->0 extrapolation  */
     
-    cout<<"Continuum limit extrapolation:"<<endl<<endl;
-    continuum_limit(rave_chir_sub_evo_ave,LO);
-    continuum_limit(rave_chir_sub_evo_ave,EM);
+    /************************************************************************/
+    /**                        a2p2->0 extrapolation                       **/
+    /************************************************************************/
+    
+//    cout<<"Continuum limit extrapolation:"<<endl<<endl;
+//    continuum_limit(rave_chir_sub_evo_ave,LO);
+//    continuum_limit(rave_chir_sub_evo_ave,EM);
 
    
 
