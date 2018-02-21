7485cc925c48419176bb507b9f7b0334a2df084c
Matteo Di Carlo <matteo@MacBook-Pro-di-Matteo-3.local>
2018-02-21 11:59:34 +0100

    ave_err for meslep and Z4f

--- bin/ave_err.hpp
+++ bin/ave_err.hpp
@@ -13,5 +13,9 @@ tuple<vvd_t,vvd_t> ave_err(vector<vvd_t> jZq);
 // average of effective mass
 tuple<vvd_t,vvd_t> ave_err(vvvd_t jM);
 
+// average meslep and Z4f
+tuple<vvvvvd_t,vvvvvd_t> ave_err(vector<jproj_meslep_t> jZ4f);
+
+
 
 #endif
