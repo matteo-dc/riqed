4bd71dd0cd538eace9953ebc7cfe595674d4e59f
Matteo Di Carlo <matteo@MBP-di-Matteo-3.lan>
2018-03-01 21:29:25 +0100

    fixed sizes

--- bin/rotate.cpp
+++ bin/rotate.cpp
@@ -8,22 +8,25 @@
 // Rotation from Physical basis to Twisted basis
 vvvprop_t rotate(vvvprop_t &S)
 {
-    vvvprop_t S_rotated(vvprop_t(vprop_t(prop_t::Zero(),nmr),ntypes),njacks);
+    int _njacks=S.size();
+    int _ntypes=S[0].size();
+    int _nmr=S[0][0].size();
+    
+    vvvprop_t S_rotated(vvprop_t(vprop_t(prop_t::Zero(),_nmr),_ntypes),_njacks);
     
     dcompl I(0.0,1.0);
     
-#pragma omp parallel for collapse(3)
-    for(int m=0;m<nm;m++)
-        for(int t=0;t<ntypes;t++)
-                for(int ijack=0;ijack<njacks;ijack++)
-                    for(int r=0;r<nr;r++)
-                    {
-                        prop_t rot=(GAMMA[0]+I*(double)(2*r-1)*GAMMA[5])/sqrt(2);
-                        
-                        int mr = r + nr*m;
-                        
-                        S_rotated[ijack][t][mr] = rot*S[ijack][t][mr]*rot;
-                    }
+#pragma omp parallel for collapse(2)
+    for(int t=0;t<_ntypes;t++)
+        for(int ijack=0;ijack<_njacks;ijack++)
+            for(int mr=0;mr<_nmr;mr++)
+            {
+                int r = mr%nr;
+                
+                prop_t rot=(GAMMA[0]+I*(double)(2*r-1)*GAMMA[5])/sqrt(2);
+                
+                S_rotated[ijack][t][mr] = rot*S[ijack][t][mr]*rot;
+            }
     
     return S_rotated;
 }
