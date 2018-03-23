ee818c9e9c8d89f0ca27fb56e625bb8a67b3e04f
Matteo Di Carlo <matteo@MacBook-Pro-di-Matteo-3.local>
2018-03-23 12:11:38 +0100

    defining UNK in symmetrization

--- bin/contractions.cpp
+++ bin/contractions.cpp
@@ -35,27 +35,13 @@ string path_to_contr(const string &suffix, const string &out, const string &stri
         
         sprintf(path,"%s%s/%04d/mes_contr_M%s_R%d_%s_M%s_R%d_%s",string_path.c_str(),out.c_str(),i_conf,suffix.c_str(),r1,T1.c_str(),suffix.c_str(),r2,T2.c_str());
     }
-    
-    // complete path
-//    string string_path;
-    
-//    if(strcmp(action.c_str(),"Iwa")==0)
-//    {
-//        string_path = path_ensemble_str+action+"_b"+to_string_with_precision(beta,2)+"_L"+to_string(size[1])+"T"+to_string(size[0])+"_k"+to_string_with_precision(kappa,6)+"_mu"+to_string_with_precision(mu_sea,4)+"/";
-//    }
-//    if(strcmp(action.c_str(),"Sym")==0)
-//    {
-//        string_path = path_ensemble_str+to_string_with_precision(beta,2)+"_"+to_string(size[1])+"_"+to_string_with_precision(mu_sea,4)+"/";
-//    }
-
-    // cout<<path<<endl;
-    
+        
     return path;
 }
 
 
 //get the contraction from file
-vvd_t get_contraction(const string &suffix, const string &out, const int mr1, const string &T1, const int mr2, const string &T2, const string &ID, const string &reim, const string &parity/*, const int T, const int nconfs, const int njacks*/ , const int* conf_id , const string &string_path)
+vvd_t get_contraction(const string &suffix, const string &out, const int mr1, const string &T1, const int mr2, const string &T2, const string &ID, const string &reim, const string &parity, const int* conf_id , const string &string_path)
 {
     
     int T=size[0];
@@ -127,7 +113,7 @@ vvd_t get_contraction(const string &suffix, const string &out, const int mr1, co
         infile.close();
     }
         
-    jV0P5_real=jackknife/*_double*/(jV0P5_real,T/*,nconfs,clust_size*/);
+    jV0P5_real=jackknife(jV0P5_real,T);
     jV0P5_imag=jackknife(jV0P5_imag,T);
     jP5P5_real=jackknife(jP5P5_real,T);
     jP5P5_imag=jackknife(jP5P5_imag,T);
@@ -143,6 +129,7 @@ vvd_t get_contraction(const string &suffix, const string &out, const int mr1, co
     
     if(parity=="EVEN") par=1.0;
     if(parity=="ODD") par=-1.0;
+    if(parity=="UNK") par=0.0;
     
     vvd_t jvec_sym(vd_t(0.0,T),njacks);
     vvd_t jvec_par(vd_t(0.0,T/2+1),njacks);
@@ -152,20 +139,8 @@ vvd_t get_contraction(const string &suffix, const string &out, const int mr1, co
         for(int t=0;t<T;t++)
             jvec_sym[ijack][(T-t)%T]=jvec[ijack][t];
         for(int t=0;t<T/2+1;t++)
-            jvec_par[ijack][t]=(jvec[ijack][t]+par*jvec_sym[ijack][t])/2.0;
+            jvec_par[ijack][t]=(jvec[ijack][t]+par*jvec_sym[ijack][t])/(1+abs(par));
     }
-    
-//    string path=ID+"_"+reim+"_mrbw_"+to_string(mr1)+"_mrfw_"+to_string(mr2)+"_"+T1+T2+".xmg";
-//    ofstream out(path);
-//    for(int t=0;t<T;t++)
-//        out<<t<<" "<<jvec_sym[0][t]<<endl;
-    
-    // if(ID=="P5P5" and reim=="RE" and parity=="EVEN"){
-    //   cout<<"**********DEBUG*************"<<endl;
-    //   for(int ijack=0;ijack<njacks;ijack++)
-    //     for(int t=0;t<T;t++)
-    // 	cout<<jvec[ijack][t]<<endl;
-    //   cout<<"**********DEBUG*************"<<endl;}
     
     return jvec_par;
     
