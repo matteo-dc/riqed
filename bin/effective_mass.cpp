e1436edf6f874ad862e40fdcb00cb96e80638de7
Matteo Di Carlo <matteo@dot1x201.roma1.infn.it>
2018-04-05 11:12:30 +0200

    computing mPCAC

--- bin/effective_mass.cpp
+++ bin/effective_mass.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <omp.h>
 #include "operations.hpp"
+#include "tools.hpp"
+#include "ave_err.hpp"
 
 //function to use in Newton's method for M_eff
 double f_mass (int t, int T, double x0, double y)
@@ -75,6 +77,144 @@ double solve_Newton (vvd_t C, int ijack, int t, const int T)
         
         return x1;
     }
+}
+
+// compute mPCAC
+void oper_t::compute_mPCAC(const string &suffix)
+{
+    int _nmr=0, _nm=0;
+    if(suffix.compare("")==0)
+    {
+        _nm=nm;
+        _nmr=nmr;
+    }
+    else if(suffix.compare("sea")==0)
+    {
+        _nm=1;
+        _nmr=nr;
+    }
+    
+    // array of the configurations
+    int conf_id[nconfs];
+    for(int iconf=0;iconf<nconfs;iconf++)
+        conf_id[iconf]=conf_init+iconf*conf_step;
+    
+    int T=size[0];
+    
+    // define jackknife V0P5 correlators
+    vvvvd_t jP5P5_00(vvvd_t(vvd_t(vd_t(T/2+1),njacks),_nmr),_nmr);
+    vvvvd_t jV0P5_00(vvvd_t(vvd_t(vd_t(T/2+1),njacks),_nmr),_nmr);
+    
+    vvvvd_t jV0P5_00_ave(vvvd_t(vvd_t(vd_t(0.0,T/2+1),njacks),_nm),_nm);
+    vvvvd_t jP5P5_00_ave(vvvd_t(vvd_t(vd_t(0.0,T/2+1),njacks),_nm),_nm);
+    
+    vvvvd_t mPCAC_corr(vvvd_t(vvd_t(vd_t(0.0,T/2+1),njacks),_nm),_nm);
+    vvvvd_t mPCAC_corr_symm(vvvd_t(vvd_t(vd_t(0.0,T/2+1),njacks),_nm),_nm);
+    
+    vvvd_t mPCAC_corr_ave(vvd_t(vd_t(0.0,T/2+1),_nm),_nm);
+    vvvd_t mPCAC_corr_symm_ave(vvd_t(vd_t(0.0,T/2+1),_nm),_nm);
+    vvvd_t sqr_mPCAC_corr_ave(vvd_t(vd_t(0.0,T/2+1),_nm),_nm);
+    vvvd_t sqr_mPCAC_corr_symm_ave(vvd_t(vd_t(0.0,T/2+1),_nm),_nm);
+    vvvd_t mPCAC_corr_err(vvd_t(vd_t(0.0,T/2+1),_nm),_nm);
+    vvvd_t mPCAC_corr_symm_err(vvd_t(vd_t(0.0,T/2+1),_nm),_nm);
+
+    for(int mr_fw=0;mr_fw<_nmr;mr_fw++)
+        for(int mr_bw=0;mr_bw<_nmr;mr_bw++)
+        {
+            jP5P5_00[mr_fw][mr_bw]=get_contraction(suffix,out_hadr,mr_fw,"0",mr_bw,"0","P5P5","RE","EVEN",conf_id,path_to_ens);
+            jV0P5_00[mr_fw][mr_bw]=get_contraction(suffix,out_hadr,mr_fw,"0",mr_bw,"0","V0P5","RE","ODD",conf_id,path_to_ens);
+        }
+    
+    //average over r
+#pragma omp parallel for collapse(4)
+    for(int m_fw=0;m_fw<_nm;m_fw++)
+        for(int m_bw=0;m_bw<_nm;m_bw++)
+            for (int ijack=0; ijack<njacks; ijack++)
+                for(int t=0;t<T/2+1;t++)
+                    for(int r=0;r<nr;r++)
+                    {
+                        int r1=r;
+                        int r2=(r+1)%2;
+                        
+                        int cr_even = r2 + r1;
+                        int cr_odd = r2 - r1;
+                        
+                        jV0P5_00_ave[m_fw][m_bw][ijack][t] += jV0P5_00[r1+nr*m_fw][r1+nr*m_bw][ijack][t]*cr_odd/nr;
+                        jP5P5_00_ave[m_fw][m_bw][ijack][t] += jP5P5_00[r1+nr*m_fw][r1+nr*m_bw][ijack][t]*cr_even/nr;
+                    }
+
+#pragma omp parallel for collapse(4)
+    for(int m_fw=0;m_fw<_nm;m_fw++)
+        for(int m_bw=0;m_bw<_nm;m_bw++)
+            for (int ijack=0; ijack<njacks; ijack++)
+                for(int t=0;t<T/2+1;t++)
+                    {
+                        mPCAC_corr[m_fw][m_bw][ijack][t] = forward_derivative(jV0P5_00_ave[m_fw][m_bw][ijack])[t]/(2.0*jP5P5_00_ave[m_fw][m_bw][ijack][t]);
+                        mPCAC_corr_symm[m_fw][m_bw][ijack][t] = symmetric_derivative(jV0P5_00_ave[m_fw][m_bw][ijack])[t]/(2.0*jP5P5_00_ave[m_fw][m_bw][ijack][t]);
+                    }
+    
+#pragma omp parallel for collapse(3)
+    for(int m_fw=0;m_fw<_nm;m_fw++)
+        for(int m_bw=0;m_bw<_nm;m_bw++)
+            for(int t=0;t<T/2;t++)
+            {
+                for(int ijack=0;ijack<njacks;ijack++)
+                {
+                    mPCAC_corr_ave[m_fw][m_bw][t]+=mPCAC_corr[m_fw][m_bw][ijack][t]/njacks;
+                    sqr_mPCAC_corr_ave[m_fw][m_bw][t]+=mPCAC_corr[m_fw][m_bw][ijack][t]*mPCAC_corr[m_fw][m_bw][ijack][t]/njacks;
+                    
+                    mPCAC_corr_symm_ave[m_fw][m_bw][t]+=mPCAC_corr_symm[m_fw][m_bw][ijack][t]/njacks;
+                    sqr_mPCAC_corr_symm_ave[m_fw][m_bw][t]+=mPCAC_corr_symm[m_fw][m_bw][ijack][t]*mPCAC_corr_symm[m_fw][m_bw][ijack][t]/njacks;
+                }
+                
+                mPCAC_corr_err[m_fw][m_bw][t]=sqrt((double)(njacks-1))*sqrt(fabs(sqr_mPCAC_corr_ave[m_fw][m_bw][t]-mPCAC_corr_ave[m_fw][m_bw][t]*mPCAC_corr_ave[m_fw][m_bw][t]));
+                mPCAC_corr_symm_err[m_fw][m_bw][t]=sqrt((double)(njacks-1))*sqrt(fabs(sqr_mPCAC_corr_symm_ave[m_fw][m_bw][t]-mPCAC_corr_symm_ave[m_fw][m_bw][t]*mPCAC_corr_symm_ave[m_fw][m_bw][t]));
+            }
+
+    
+    //t-range for the fit
+    int t_min = delta_tmin;
+    int t_max = delta_tmax;
+    
+    vvd_t coord(vd_t(0.0,T/2),1);
+    for(int j=0; j<T/2; j++)
+    {
+        coord[0][j] = 1.0;  //fit a costante
+    }
+    
+    vvvvd_t mPCAC_tmp(vvvd_t(vvd_t(vd_t(0.0,coord.size()),njacks),_nm),_nm);
+    vvvvd_t mPCAC_symm_tmp(vvvd_t(vvd_t(vd_t(0.0,coord.size()),njacks),_nm),_nm);
+    
+    for(int m_fw=0;m_fw<_nm;m_fw++)
+        for(int m_bw=0;m_bw<_nm;m_bw++)
+        {
+            mPCAC_tmp[m_fw][m_bw] = polyfit(coord, 1, mPCAC_corr_err[m_fw][m_bw], mPCAC_corr[m_fw][m_bw], t_min, t_max);
+            mPCAC_symm_tmp[m_fw][m_bw] = polyfit(coord, 1, mPCAC_corr_symm_err[m_fw][m_bw], mPCAC_corr_symm[m_fw][m_bw], t_min, t_max);
+        }
+    
+    vvvd_t mPCAC(vvd_t(vd_t(0.0,_nm),_nm),njacks);
+    vvvd_t mPCAC_symm(vvd_t(vd_t(0.0,_nm),_nm),njacks);
+    
+#pragma omp parallel for collapse(3)
+    for(int ijack=0;ijack<njacks;ijack++)
+        for(int m_fw=0;m_fw<_nm;m_fw++)
+            for(int m_bw=0;m_bw<_nm;m_bw++)
+            {
+                mPCAC[ijack][m_fw][m_bw]=mPCAC_tmp[m_fw][m_bw][ijack][0];
+                mPCAC_symm[ijack][m_fw][m_bw]=mPCAC_symm_tmp[m_fw][m_bw][ijack][0];
+            }
+    
+    for(int m=0;m<_nm;m++)
+    {
+        string mass;
+        if(_nm==1) mass="sea";
+        else mass=to_string(m);
+        cout<<"mPCAC["<<mass<<"] = "<<get<0>(ave_err(mPCAC))[m][m]<<" "<<get<1>(ave_err(mPCAC))[m][m]<<endl;
+        cout<<"mPCAC_symm["<<mass<<"] = "<<get<0>(ave_err(mPCAC_symm))[m][m]<<" "<<get<1>(ave_err(mPCAC_symm))[m][m]<<endl;
+    }
+
+    cout<<endl;
+
 }
 
 // compute effective mass
@@ -296,4 +436,6 @@ void oper_t::compute_eff_mass_sea()
     }
     else cerr<<"Unable to create the output file \"eff_mass_sea_array\" "<<endl;
 }
+
+
 
--- bin/effective_mass.hpp
+++ bin/effective_mass.hpp
@@ -1,6 +1,5 @@
 #ifndef _EFF_MASS_HPP
 #define _EFF_MASS_HPP
 
-//void compute_eff_mass();
 
 #endif
