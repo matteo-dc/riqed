0f176a06ab6405685203efe1fbb0192ff390528e
Matteo Di Carlo <matteo@MBP-di-Matteo-3.lan>
2018-04-08 23:26:57 +0200

    simplifying pr_bil computation

--- bin/Zbil.cpp
+++ bin/Zbil.cpp
@@ -13,36 +13,30 @@ vvvvvd_t compute_pr_bil( vvvprop_t &jpropOUT_inv,  valarray<jvert_t> &jVert,  vv
     const int iv[4]={LO,EM,LO,LO};
     const int i2[4]={LO,LO,LO,EM};
     
-    valarray<jvert_t> jLambda(vvvvprop_t(vvvprop_t(vvprop_t(vprop_t(prop_t::Zero(),16),nmr),nmr),njacks),4);
-
+    int npr_bil = 4;
+    
     valarray<jproj_t> jG(vvvvd_t(vvvd_t(vvd_t(vd_t(0.0,nmr),nmr),njacks),nbil),4);
     valarray<jproj_t> jG_LO_and_EM(vvvvd_t(vvvd_t(vvd_t(vd_t(0.0,nmr),nmr),njacks),nbil),2);
+    
+    const int ibil_of_igam[16]={0,1,1,1,1,2,3,3,3,3,4,4,4,4,4,4};
     
 #pragma omp parallel for collapse(5)
     for(int ijack=0;ijack<njacks;ijack++)
         for(int mr_fw=0;mr_fw<nmr;mr_fw++)
             for(int mr_bw=0;mr_bw<nmr;mr_bw++)
                 for(int igam=0;igam<16;igam++)
-                    for(int k=0;k<4;k++)
+                    for(int k=0;k<npr_bil;k++)
                     {
-                        jLambda[k][ijack][mr_fw][mr_bw][igam] = jpropOUT_inv[i1[k]][ijack][mr_fw]*jVert[iv[k]][ijack][mr_fw][mr_bw][igam]*GAMMA[5]*(jpropIN_inv[i2[k]][ijack][mr_bw]).adjoint()*GAMMA[5];
+                        prop_t jLambda_igam = jpropOUT_inv[i1[k]][ijack][mr_fw]*
+                                              jVert[iv[k]][ijack][mr_fw][mr_bw][igam]*
+                                              GAMMA[5]*(jpropIN_inv[i2[k]][ijack][mr_bw]).adjoint()*GAMMA[5];
                         
-                        if(igam==0) jG[k][0][ijack][mr_fw][mr_bw]=(jLambda[k][ijack][mr_fw][mr_bw][0]*Proj[0]).trace().real()/12.0;
-                        if(igam>0 and igam<5) jG[k][1][ijack][mr_fw][mr_bw]+=(jLambda[k][ijack][mr_fw][mr_bw][igam]*Proj[igam]).trace().real()/12.0;
-                        if(igam==5) jG[k][2][ijack][mr_fw][mr_bw]=(jLambda[k][ijack][mr_fw][mr_bw][5]*Proj[5]).trace().real()/12.0;
-                        if(igam>5 and igam<10) jG[k][3][ijack][mr_fw][mr_bw]+=(jLambda[k][ijack][mr_fw][mr_bw][igam]*Proj[igam]).trace().real()/12.0;
-                        if(igam>=10 and igam<16) jG[k][4][ijack][mr_fw][mr_bw]+=(jLambda[k][ijack][mr_fw][mr_bw][igam]*Proj[igam]).trace().real()/12.0;
+                        jG[k][ibil_of_igam[igam]][ijack][mr_fw][mr_bw] +=
+                            (jLambda_igam*Proj[igam]).trace().real()/12.0;
                     }
     
     jG_LO_and_EM[LO] = jG[0];
     jG_LO_and_EM[EM] = jG[1]-jG[2]-jG[3];  // jG_em = -jG_1+jG_a+jG_b;
-    
-    printf("__V/A__\n");
-    for(int k=0; k<4;k++)
-        {
-            printf("%lg \n",(jLambda[k][0][0][0][1]*Proj[6]).trace().real()/12.0);
-        }
-    printf("____\n");
     
     return jG_LO_and_EM;
 }
