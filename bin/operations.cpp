796788850616cb302eac494728eec78d89ddcc93
Matteo Di Carlo <matteo@dot1x201.roma1.infn.it>
2018-04-11 18:39:14 +0200

    printing only QCD amputated with QED

--- bin/operations.cpp
+++ bin/operations.cpp
@@ -964,7 +964,7 @@ void oper_t::compute_meslep()
             jvproj_meslep_t jpr_meslep = compute_pr_meslep(jS1_inv_LO_and_EM,jmeslep,jS2_inv_LO_and_EM,qIN,qOUT,ql);
             
             jpr_meslep_0[imeslepmom] = jpr_meslep[QCD];
-            jpr_meslep_em[imeslepmom] = /*jpr_meslep[M11] + jpr_meslep[M22]*/ + jpr_meslep[M12] /*- jpr_meslep[6] - jpr_meslep[7]*/;
+            jpr_meslep_em[imeslepmom] = /*jpr_meslep[M11] + jpr_meslep[M22] + jpr_meslep[M12]*/ - jpr_meslep[6] - jpr_meslep[7];
             jpr_meslep_nasty[imeslepmom] = jpr_meslep[IN] + jpr_meslep[OUT];
             
             ////////////////////////////////////////////////////////////
