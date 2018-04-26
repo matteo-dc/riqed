3c245940aeed9e50e0a18250895c6a04bbff9bbd
Matteo Di Carlo <matteo@MacBook-Pro-di-Matteo-3.local>
2018-04-26 19:44:31 +0200

    making order

--- bin/deltam_cr.cpp
+++ bin/deltam_cr.cpp
@@ -62,28 +62,7 @@ void oper_t::compute_deltam()
     
     vvvvd_t jP5P5_S(vvvd_t(vvd_t(vd_t(T),njacks),nmr),nmr);
 
-//    //define jackknife V0P5 correlators with a unique r dependence
-//    vvvvvd_t jV0P5_LL_ave(vvvvd_t(vvvd_t(vvd_t(vd_t(0.0,T/2+1),njacks),nr),nm),nm);
-//    vvvvvd_t jV0P5_0M_ave(vvvvd_t(vvvd_t(vvd_t(vd_t(0.0,T/2+1),njacks),nr),nm),nm);
-//    vvvvvd_t jV0P5_M0_ave(vvvvd_t(vvvd_t(vvd_t(vd_t(0.0,T/2+1),njacks),nr),nm),nm);
-//    vvvvvd_t jV0P5_0T_ave(vvvvd_t(vvvd_t(vvd_t(vd_t(0.0,T/2+1),njacks),nr),nm),nm);
-//    vvvvvd_t jV0P5_T0_ave(vvvvd_t(vvvd_t(vvd_t(vd_t(0.0,T/2+1),njacks),nr),nm),nm);
-//    vvvvvd_t jV0P5_0P_ave(vvvvd_t(vvvd_t(vvd_t(vd_t(0.0,T/2+1),njacks),nr),nm),nm);
-//    vvvvvd_t jV0P5_P0_ave(vvvvd_t(vvvd_t(vvd_t(vd_t(0.0,T/2+1),njacks),nr),nm),nm);
-//
-//    //define jackknife P5P5 correlators with a unique r dependence
-//    vvvvvd_t jP5P5_LL_ave(vvvvd_t(vvvd_t(vvd_t(vd_t(0.0,T/2+1),njacks),nr),nm),nm);
-//    vvvvvd_t jP5P5_0M_ave(vvvvd_t(vvvd_t(vvd_t(vd_t(0.0,T/2+1),njacks),nr),nm),nm);
-//    vvvvvd_t jP5P5_M0_ave(vvvvd_t(vvvd_t(vvd_t(vd_t(0.0,T/2+1),njacks),nr),nm),nm);
-//    vvvvvd_t jP5P5_0T_ave(vvvvd_t(vvvd_t(vvd_t(vd_t(0.0,T/2+1),njacks),nr),nm),nm);
-//    vvvvvd_t jP5P5_T0_ave(vvvvd_t(vvvd_t(vvd_t(vd_t(0.0,T/2+1),njacks),nr),nm),nm);
-//    vvvvvd_t jP5P5_0P_ave(vvvvd_t(vvvd_t(vvd_t(vd_t(0.0,T/2+1),njacks),nr),nm),nm);
-//    vvvvvd_t jP5P5_P0_ave(vvvvd_t(vvvd_t(vvd_t(vd_t(0.0,T/2+1),njacks),nr),nm),nm);
-
     //define deltam_cr
-//    vvvvvd_t num_deltam_cr_corr(vvvvd_t(vvvd_t(vvd_t(vd_t(0.0,T/2+1),njacks),nr),nm),nm);
-//    vvvvvd_t den_deltam_cr_corr(vvvvd_t(vvvd_t(vvd_t(vd_t(0.0,T/2+1),njacks),nr),nm),nm);
-    
     vvvvvd_t v_deltamc(vvvvd_t(vvvd_t(vvd_t(vd_t(0.0,T/2+1),njacks),nr),nm),nm);
     vvvvvd_t v_deltamu(vvvvd_t(vvvd_t(vvd_t(vd_t(0.0,T/2+1),njacks),nr),nm),nm);
     
@@ -158,70 +137,6 @@ void oper_t::compute_deltam()
                                                  jP5P5_S0[mr_fw][mr_bw][ijack];
             }
     
-//    //average over r
-//#pragma omp parallel for collapse(4)
-//    for(int m_fw=0;m_fw<nm;m_fw++)
-//        for(int m_bw=0;m_bw<nm;m_bw++)
-//            for (int ijack=0; ijack<njacks; ijack++)
-//                for(int t=0;t<T/2+1;t++)
-//                    for(int r=0;r<nr;r++)
-//                    {
-//                        int r1=r;
-//                        int r2=(r+1)%2;
-//
-//                        int cr_even = r2 + r1;
-//                        int cr_odd = r2 - r1;
-//
-//                        jV0P5_LL_ave[m_fw][m_bw][ijack][t] += jV0P5_LL[r1+nr*m_fw][r1+nr*m_bw][ijack][t]*cr_odd/nr;
-//                        jV0P5_0M_ave[m_fw][m_bw][ijack][t] += jV0P5_0M[r1+nr*m_fw][r1+nr*m_bw][ijack][t]*cr_odd/nr;
-//                        jV0P5_M0_ave[m_fw][m_bw][ijack][t] += jV0P5_M0[r1+nr*m_fw][r1+nr*m_bw][ijack][t]*cr_odd/nr;
-//                        jV0P5_0T_ave[m_fw][m_bw][ijack][t] += jV0P5_0T[r1+nr*m_fw][r1+nr*m_bw][ijack][t]*cr_odd/nr;
-//                        jV0P5_T0_ave[m_fw][m_bw][ijack][t] += jV0P5_T0[r1+nr*m_fw][r1+nr*m_bw][ijack][t]*cr_odd/nr;
-//                        jV0P5_0P_ave[m_fw][m_bw][ijack][t] += jV0P5_0P[r1+nr*m_fw][r1+nr*m_bw][ijack][t]*cr_even/nr;
-//                        jV0P5_P0_ave[m_fw][m_bw][ijack][t] += jV0P5_P0[r1+nr*m_fw][r1+nr*m_bw][ijack][t]*cr_even/nr;
-//                    }
-    
-//    // same r but without average
-//#pragma omp parallel for collapse(5)
-//    for(int m_fw=0;m_fw<nm;m_fw++)
-//        for(int m_bw=0;m_bw<nm;m_bw++)
-//            for (int ijack=0; ijack<njacks; ijack++)
-//                for(int t=0;t<T/2+1;t++)
-//                    for(int r=0;r<nr;r++)
-//                    {
-//                        int r1=r;
-//                        int r2=(r+1)%2;
-//                        
-//                        jV0P5_LL_ave[m_fw][m_bw][r][ijack][t] = jV0P5_LL[r1+nr*m_fw][r1+nr*m_bw][ijack][t];
-//                        jV0P5_0M_ave[m_fw][m_bw][r][ijack][t] = jV0P5_0M[r1+nr*m_fw][r1+nr*m_bw][ijack][t];
-//                        jV0P5_M0_ave[m_fw][m_bw][r][ijack][t] = jV0P5_M0[r1+nr*m_fw][r1+nr*m_bw][ijack][t];
-//                        jV0P5_0T_ave[m_fw][m_bw][r][ijack][t] = jV0P5_0T[r1+nr*m_fw][r1+nr*m_bw][ijack][t];
-//                        jV0P5_T0_ave[m_fw][m_bw][r][ijack][t] = jV0P5_T0[r1+nr*m_fw][r1+nr*m_bw][ijack][t];
-//                        jV0P5_0P_ave[m_fw][m_bw][r][ijack][t] = jV0P5_0P[r1+nr*m_fw][r1+nr*m_bw][ijack][t];
-//                        jV0P5_P0_ave[m_fw][m_bw][r][ijack][t] = jV0P5_P0[r1+nr*m_fw][r1+nr*m_bw][ijack][t];
-//                    }
-    
-   
-//    // taking only opposite r (opposite r's become same r's due to the dagger):
-//    //   in Nf=4 analysis we are not at maximal twist and the leading order m_cr wouldn't be zero otherwise.
-//#pragma omp parallel for collapse(4)
-//    for(int m_fw=0;m_fw<nm;m_fw++)
-//        for(int m_bw=0;m_bw<nm;m_bw++)
-//            for (int ijack=0; ijack<njacks; ijack++)
-//                for(int t=0;t<T/2+1;t++)
-//                    for(int r=0; r<1; r++)
-//                    {
-//                        int r1=r;
-//                        int r2=(r+1)%2;
-//                        
-//                        jV0P5_LL_ave[m_fw][m_bw][ijack][t] = jV0P5_LL[r1+nr*m_fw][r2+nr*m_bw][ijack][t];
-//                        jV0P5_0M_ave[m_fw][m_bw][ijack][t] = jV0P5_0M[r1+nr*m_fw][r2+nr*m_bw][ijack][t];
-//                        jV0P5_M0_ave[m_fw][m_bw][ijack][t] = jV0P5_M0[r1+nr*m_fw][r2+nr*m_bw][ijack][t];
-//                        jV0P5_0T_ave[m_fw][m_bw][ijack][t] = jV0P5_0T[r1+nr*m_fw][r2+nr*m_bw][ijack][t];
-//                        jV0P5_T0_ave[m_fw][m_bw][ijack][t] = jV0P5_T0[r1+nr*m_fw][r2+nr*m_bw][ijack][t];
-//                        jV0P5_0P_ave[m_fw][m_bw][ijack][t] = jV0P5_0P[r1+nr*m_fw][r2+nr*m_bw][ijack][t];
-//                        jV0P5_P0_ave[m_fw][m_bw][ijack][t] = jV0P5_P0[r1+nr*m_fw][r2+nr*m_bw][ijack][t];
-//                    }
     
     vvvvd_t mean_value_mu(vvvd_t(vvd_t(vd_t(0.0,T/2+1),nr),nm),nm), sqr_mean_value_mu(vvvd_t(vvd_t(vd_t(0.0,T/2+1),nr),nm),nm), error_mu(vvvd_t(vvd_t(vd_t(0.0,T/2+1),nr),nm),nm);
     vvvvd_t mean_value_mc(vvvd_t(vvd_t(vd_t(0.0,T/2+1),nr),nm),nm), sqr_mean_value_mc(vvvd_t(vvd_t(vd_t(0.0,T/2+1),nr),nm),nm), error_mc(vvvd_t(vvd_t(vd_t(0.0,T/2+1),nr),nm),nm);
