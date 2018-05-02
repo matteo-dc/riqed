23610d697bca48e06c27e9bc66bd1c080c390a16
Matteo Di Carlo <matteo@MBP-di-Matteo-3.lan>
2018-05-02 14:00:03 +0200

    moved deltam after computation of sigma

--- bin/operations.cpp
+++ bin/operations.cpp
@@ -230,8 +230,6 @@ void oper_t::create_basic(const int b, const int th, const int msea)
     if(UseEffMass) eff_mass=read_eff_mass(path_to_ens+"eff_mass_array");
     if(UseEffMass) eff_mass_time=read_eff_mass_time(path_to_ens+"eff_mass_array_time");
     if(UseEffMass and _nm_Sea>0) eff_mass_sea=read_eff_mass_sea(path_to_ens+"eff_mass_sea_array");
-    deltam_cr = read_deltam(path_to_ens,"deltam_cr_array");
-    deltamu   = read_deltam(path_to_ens,"deltamu_array");
     
     switch(get_scheme())
     {
@@ -248,6 +246,11 @@ void oper_t::create_basic(const int b, const int th, const int msea)
             break;
     }
     
+    // read or compute deltam
+    deltam_cr = read_deltam(path_to_ens,"deltam_cr_array");
+    deltamu   = read_deltam(path_to_ens,"deltamu_array");
+    
+//    compute_Zq();
 //    compute_Zbil();
 //    if(compute_4f) compute_Z4f();
     
