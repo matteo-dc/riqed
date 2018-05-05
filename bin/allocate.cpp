7e82728ae62eea696cb223905d730fcfe149acd0
Matteo Di Carlo <matteo@MBP-di-Matteo-3.lan>
2018-05-05 18:06:41 +0200

    adding allocation of eff_masses

--- bin/allocate.cpp
+++ bin/allocate.cpp
@@ -16,16 +16,20 @@ void allocate_vec_internal(double &t, const vector<int> sizes, int isize)
 
 void oper_t::allocate()
 {
+    // allocate effective masses
+    allocate_vec(eff_mass,{njacks,_nmr,_nmr});
+    allocate_vec(eff_mass_sea,{njacks,_nr,_nr});
+    
+    // allocate deltam
+    allocate_vec(deltam_cr,{njacks,_nmr});
+    allocate_vec(deltamu,{njacks,_nmr});
+    
     // allocate sigmas
     allocate_vec(sigma,{_linmoms,sigma::nproj,sigma::nins,njacks,_nmr});
     // allocate pr_bil
     allocate_vec(jG,{_bilmoms,gbil::nins,nbil,njacks,_nmr,_nmr});
     // allocate pr_meslep
     allocate_vec(jpr_meslep,{_meslepmoms,pr_meslep::nins,nbil,nbil,njacks,_nmr,_nmr});
-    
-    // allocate deltam
-    allocate_vec(deltam_cr,{njacks,_nmr});
-    allocate_vec(deltamu,{njacks,_nmr});
     
     // allocate Zq
     allocate_vec(jZq,{_linmoms,njacks,_nmr});
